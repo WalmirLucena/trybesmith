1d5756c7407d12e9924c05c73ad06ee30fd16cc0
WalmirLucena <walmirlucena3@gmail.com>
2022-02-23 18:37:31 -0400

    Refatora Model de Product

--- src/controller/ProductController.ts
+++ src/controller/ProductController.ts
@@ -7,8 +7,10 @@ const create = async (req: Request, res: Response) => {
   const { name, amount } = req.body;
 
   const result: Product = await ProductService.create(name, amount);
+    
+  const { id } = result;
       
-  return res.status(StatusCode.CREATED).json({ item: result });
+  return res.status(StatusCode.CREATED).json({ item: { id, name, amount } });
 };
 
 export default { create };
--- src/models/ProductModel.ts
+++ src/models/ProductModel.ts
@@ -1,14 +1,16 @@
+import { ResultSetHeader } from 'mysql2';
 import { IProduct, Product } from '../interface/Product';
 import connection from './connection';
 
 const createProduct = async ({ name, amount }: IProduct): Promise<Product> => {
   const query = 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?,?)';
   const [result] = await connection
-    .execute(query, [name, amount]);
+    .execute<ResultSetHeader>(query, [name, amount]);
   
-  const [row] = result as Product[];
-
-  return row;
+  const { insertId: id } = result;
+  const insertedProduct: Product = { id, name, amount };
+    
+  return insertedProduct;
 };
 
 export default { createProduct };

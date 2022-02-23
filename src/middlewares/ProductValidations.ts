5bbbba8997fa2cc295a1a085a123e512d3232919
WalmirLucena <walmirlucena3@gmail.com>
2022-02-23 19:57:18 -0400

    Adiciona Validations de Amount e Name

--- src/middlewares/ProductValidations.ts
+++ src/middlewares/ProductValidations.ts
@@ -18,4 +18,46 @@ export const validateToken = async (req: Request, res: Response, next: NextFunct
   next();
 };
 
-export default { validateToken };
+export const validateName = async (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.body;
+  
+  if (!name) {
+    return res
+      .status(StatusCode.BAD_REQUEST).send({ error: 'Name is required' });
+  }
+
+  if (typeof name !== 'string') {
+    return res
+      .status(StatusCode.UNPROCESSABLE_ENTITY).send({ error: 'Name must be a string' });
+  }
+
+  if (name.length < 3) {
+    return res
+      .status(StatusCode.UNPROCESSABLE_ENTITY)
+      .send({ error: 'Name must be longer than 2 characters' });
+  }
+
+  next();
+};
+
+export const validateAmount = async (req: Request, res: Response, next: NextFunction) => {
+  const { amount } = req.body;
+  
+  if (!amount) {
+    return res
+      .status(StatusCode.BAD_REQUEST).send({ error: 'Amount is required' });
+  }
+
+  if (typeof amount !== 'string') {
+    return res
+      .status(StatusCode.UNPROCESSABLE_ENTITY).send({ error: 'Amount must be a string' });
+  }
+
+  if (amount.length < 3) {
+    return res
+      .status(StatusCode.UNPROCESSABLE_ENTITY)
+      .send({ error: 'Amount must be longer than 2 characters' });
+  }
+
+  next();
+};
--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import ProductController from '../controller/ProductController';
-import { validateToken } from '../middlewares/ProductValidations';
+import { validateToken, validateName, validateAmount } from '../middlewares/ProductValidations';
 
 const routerProducts = express.Router();
 
-routerProducts.post('/', validateToken, ProductController.create);
+routerProducts.post('/', validateToken, validateName, validateAmount, ProductController.create);
 
 export default routerProducts;
